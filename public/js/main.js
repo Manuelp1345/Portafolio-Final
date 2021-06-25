eceff84d3bf130f3ac3d5d60a5b94222072f3ff3
manuelp1345 <manuelp1345@gmail.com>
2021-06-24 22:34:36 -0400

    Cambio en la seccion de portafolios

--- public/js/main.js
+++ public/js/main.js
@@ -1,177 +1,166 @@
-$(document).ready(function() {
-    if ($("body,html").scrollTop != 0) {
-        $("body,html").animate({
-            scrollTop: 0
-        }, 1000, 'swing');
+const web1 = document.querySelector("#web1"),
+  web2 = document.querySelector("#web2"),
+  web3 = document.querySelector("#web3"),
+  web4 = document.querySelector("#web4"),
+  thumbnail = document.getElementsByClassName("thumbnail"),
+  webs = $("#webs");
 
-    }
+let sliderAutomatic = setInterval(slider, 3000),
+  state = false,
+  count = 0;
 
-    let port = $("#port").offset().top,
-        skills = $("#skilss").offset().top,
-        contacto = $("#contacto").offset().top;
-
-
-    $("#portafolio").on("click", (e) => {
-        e.preventDefault()
-        $("body,html").animate({
-            scrollTop: port - 130
-        }, 1000, 'swing');
-    })
-    $("#skill").on("click", (e) => {
-        e.preventDefault()
-        $("body,html").animate({
-            scrollTop: skills - 50
-        }, 1000, 'swing');
-    })
-    $("#contact").on("click", (e) => {
-        e.preventDefault()
-        $("body,html").animate({
-            scrollTop: contacto - 80
-        }, 1000, 'swing');
-    })
-    $("#inicio").on("click", (e) => {
-        e.preventDefault()
-        $("body,html").animate({
-            scrollTop: 0
-        }, 1000, 'swing');
-    })
-
-    window.onresize = () => {
-        port = $("#port").offset().top,
-            skills = $("#skilss").offset().top,
-            contacto = $("#contacto").offset().top;
-
-    }
-
-
-
-    tippy('#html', {
-        content: 'HTML5',
-        arrow: true,
-        animation: 'fade',
-        inertia: true,
-        placement: 'right',
-    });
-    tippy('#CSS', {
-        content: 'CSS',
-        arrow: true,
-        animation: 'fade',
-        inertia: true,
-        placement: 'right',
-    });
-    tippy('#JavaScript', {
-        content: 'JavaScript',
-        arrow: true,
-        animation: 'fade',
-        inertia: true,
-        placement: 'right',
-    });
-    tippy('#Jquery', {
-        content: 'Jquery',
-        arrow: true,
-        animation: 'fade',
-        inertia: true,
-        placement: 'right',
-    });
-    tippy('#node', {
-        content: 'Node.js/Express',
-        arrow: true,
-        animation: 'fade',
-        inertia: true,
-        placement: 'right',
-    });
-    tippy('#BootsTrap', {
-        content: 'BootsTrap',
-        arrow: true,
-        animation: 'fade',
-        inertia: true,
-        placement: 'right',
-    });
-
-    $("#slide_nav_button").click(function() {
-        $('#slide_menu').animate({
-            width: 'toggle',
-            height: "toggle"
-        }, 500);
-    });
-
-    if (screen.availWidth < 767) {
-        $('#slide_menu li').click(function() {
-            $('#slide_menu').animate({
-                width: 'toggle',
-                height: "toggle"
-            }, 500);
-
-        });
-    }
-
-    $("#FormContact").on("submit", (e) => {
-        e.preventDefault()
-    })
-
-    $("#FormBtn").click(() => {
-        let nombre = $("#nombre").val(),
-            email = $("#email").val(),
-            cell = $("#cell").val(),
-            msg = $("#msg").val()
-
-        if ((nombre == "") || (email == "") || (cell == "") || (msg == "")) {
-            error = 1
-            $("#error").toggle("slow")
-            $("#error").text("Por favor rellene todos los campos")
-            $("#FormBtn").attr("disabled", true)
-            setTimeout(() => {
-                $("#error").toggle("slow")
-                $("#FormBtn").attr("disabled", false)
-            }, 3000);
-
-        } else {
-            $("#FormBtn").attr("disabled", true)
-            $("#FormBtn").text("")
-            $("#FormBtn").append(`<div id="load" class=" spinner-border text-light" role="status"></div>`)
-            email = {
-                nombre,
-                email,
-                cell,
-                msg
-            }
-
-            fetch("https://contacto.manuelp1345.vercel.app/api/contact", {
-                    method: 'POST',
-                    mode: "cors",
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "Accept": "*/*",
-                    },
-                    body: JSON.stringify(email)
-
-                }).then(x => x.json())
-                .then(r => {
-                    if (r.ok != true) {
-                        $("#error").toggle("slow")
-                        $("#error").text("Por favor rellene todos los campos. ")
-                        $("#FormBtn").attr("disabled", true)
-                        setTimeout(() => {
-                            $("#error").toggle("slow")
-                            $("#FormBtn").attr("disabled", false)
-                        }, 3000);
-                    } else {
-                        $("#nice").toggle("slow")
-                        $("#nice").text(r.msg)
-                        setTimeout(() => {
-                            $("#nice").toggle("slow")
-                        }, 3000);
-                        $("#nombre").val("")
-                        $("#email").val("")
-                        $("#cell").val("")
-                        $("#msg").val("")
-                        $("#FormBtn").attr("disabled", false)
-                        $("#FormBtn").remove("#load")
-                        $("#FormBtn").text("Enviar")
-                    }
-                })
-        }
-
-    })
-
+webs.hover(() => {
+  state = !state;
+  if (state == true) {
+    clearInterval(sliderAutomatic);
+  } else {
+    sliderAutomatic = setInterval(slider, 3000);
+  }
 });
+
+function slider() {
+  count++;
+  selectCard(count);
+}
+
+function selectCard(n) {
+  if (n == 1) {
+    web1.classList.replace("d-block", "d-none");
+    web2.classList.replace("d-none", "d-block");
+    web3.classList.replace("d-block", "d-none");
+    web4.classList.replace("d-block", "d-none");
+    document
+      .getElementsByClassName("activeBar")[0]
+      .classList.remove("activeBar");
+    thumbnail[n].classList.add("activeBar");
+    count = n;
+  }
+  if (n == 2) {
+    web1.classList.replace("d-block", "d-none");
+    web2.classList.replace("d-block", "d-none");
+    web3.classList.replace("d-none", "d-block");
+    web4.classList.replace("d-block", "d-none");
+    document
+      .getElementsByClassName("activeBar")[0]
+      .classList.remove("activeBar");
+    thumbnail[n].classList.add("activeBar");
+    count = n;
+  }
+  if (n == 3) {
+    web1.classList.replace("d-block", "d-none");
+    web2.classList.replace("d-block", "d-none");
+    web3.classList.replace("d-block", "d-none");
+    web4.classList.replace("d-none", "d-block");
+    document
+      .getElementsByClassName("activeBar")[0]
+      .classList.remove("activeBar");
+    thumbnail[n].classList.add("activeBar");
+    count = n;
+  }
+  if (n == 4) {
+    web1.classList.replace("d-none", "d-block");
+    web2.classList.replace("d-block", "d-none");
+    web3.classList.replace("d-block", "d-none");
+    web4.classList.replace("d-block", "d-none");
+    document
+      .getElementsByClassName("activeBar")[0]
+      .classList.remove("activeBar");
+    thumbnail[0].classList.add("activeBar");
+    count = 0;
+  }
+}
+
+$("#slide_nav_button").click(function () {
+  $("#slide_menu").animate(
+    {
+      width: "toggle",
+      height: "toggle",
+    },
+    500
+  );
+});
+
+if (screen.availWidth < 767) {
+  $("#slide_menu li").click(function () {
+    $("#slide_menu").show();
+
+    $("#slide_menu").animate(
+      {
+        width: "toggle",
+        height: "toggle",
+      },
+      500
+    );
+  });
+}
+
+$("#FormContact").on("submit", (e) => {
+  e.preventDefault();
+});
+
+$("#FormBtn").click(() => {
+  let nombre = $("#nombre").val(),
+    email = $("#email").val(),
+    cell = $("#cell").val(),
+    msg = $("#msg").val();
+
+  if (nombre == "" || email == "" || cell == "" || msg == "") {
+    error = 1;
+    $("#error").toggle("slow");
+    $("#error").text("Por favor rellene todos los campos");
+    $("#FormBtn").attr("disabled", true);
+    setTimeout(() => {
+      $("#error").toggle("slow");
+      $("#FormBtn").attr("disabled", false);
+    }, 3000);
+  } else {
+    $("#FormBtn").attr("disabled", true);
+    $("#FormBtn").text("");
+    $("#FormBtn").append(
+      `<div id="load" class=" spinner-border text-light" role="status"></div>`
+    );
+    email = {
+      nombre,
+      email,
+      cell,
+      msg,
+    };
+
+    fetch("https://contacto.manuelp1345.vercel.app/api/contact", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+      },
+      body: JSON.stringify(email),
+    })
+      .then((x) => x.json())
+      .then((r) => {
+        if (r.ok != true) {
+          $("#error").toggle("slow");
+          $("#error").text("Por favor rellene todos los campos. ");
+          $("#FormBtn").attr("disabled", true);
+          setTimeout(() => {
+            $("#error").toggle("slow");
+            $("#FormBtn").attr("disabled", false);
+          }, 3000);
+        } else {
+          $("#nice").toggle("slow");
+          $("#nice").text(r.msg);
+          setTimeout(() => {
+            $("#nice").toggle("slow");
+          }, 3000);
+          $("#nombre").val("");
+          $("#email").val("");
+          $("#cell").val("");
+          $("#msg").val("");
+          $("#FormBtn").attr("disabled", false);
+          $("#FormBtn").remove("#load");
+          $("#FormBtn").text("Enviar");
+        }
+      });
+  }
+});
+
+document.querySelector("#year").innerHTML = new Date().getFullYear();
